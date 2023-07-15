89f61551400566717e6dd7c9a68c9a747cea575c
drsky <contact@drsky.ovh>
2023-07-16 00:36:36 +0200

    Added header layout and associated CSS

--- src/layouts/header/header.jsx
+++ src/layouts/header/header.jsx
@@ -1,10 +1,36 @@
+import { NavLink } from 'react-router-dom';
+import './header.scss';
+import logo from '../../assets/images/logo.svg';
+
 export function Header () {
+
+  function activeLink({isActive}) {
+    return isActive 
+      ? 'header__link-nav header__link-nav--active'
+      : 'header__link-nav';
+  }
   
   return (
     <header className="header">
-      <div className="header__logo">
-        <img src="" alt="" />
-      </div>
+      <img src={logo} className='header__logo' alt="logo kasa" aria-hidden="true"/>
+
+      <nav className="header__navbar">
+        <ul className='header__list'>
+          <li>
+            <NavLink 
+              to='/'
+              className={activeLink}
+            >Accueil</NavLink>
+          </li>
+          <li>
+            <NavLink 
+              to='/a-propos'
+              className={activeLink}
+            >A Propos</NavLink>
+          </li>
+        </ul>
+      </nav>
+
     </header>
-  )
+  );
 }
