8d5f1eaae20de6ac967723ebf4b66c3922c515aa
rick-c187 <contact@drsky.ovh>
2023-07-25 22:23:52 +0200

    add instruction for build script

--- src/app.jsx
+++ src/app.jsx
@@ -14,7 +14,6 @@ function App() {
         <Route index element={<Home title='Accueil - kasa' />} loader={loaderAllLocation}/>
         <Route path='annonce/:id' element={<Rental />} loader={loaderId}/>
         <Route path='a-propos' element={<About title='A propos - kasa' />} loader={loaderAllLocation}/>
-        <Route path='*' element={<ErrorPage title='404 - kasa' />} />
       </Route>
     ), {
       basename: '/p11/',
